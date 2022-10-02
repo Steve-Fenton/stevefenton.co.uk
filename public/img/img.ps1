39a6b4f8e913870230676192bcdcf82740dec570
Steve Fenton <github@stevefenton.co.uk>
2022-10-02 14:03:49 +0100

    Removed scaled images

--- public/img/img.ps1
+++ public/img/img.ps1
@@ -2,9 +2,7 @@ $files = Get-ChildItem -Recurse
 
 foreach($file in $files) {
     $name = $file.Name
-    Write-host $file.Path
-
-    if ($name -match 'x[0-9]+.jpg') {
+    if ($name -match '-scaled.png') {
         $file.Delete()
     }
 }
