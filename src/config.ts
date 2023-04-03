bad1f3d8ca9aa6e03b4f23f653104260571c8020
Steve Fenton <Steve-Fenton@users.noreply.github.com>
2023-04-03 14:52:43 +0000

    Enable bookmark links

--- src/config.ts
+++ src/config.ts
@@ -34,6 +34,7 @@ export const SITE = {
 		codeBlocks: ['copy'],
 		figures: ['enlarge'],
 		youTubeLinks: ['embed'],
+		headers: ['link'],
 	},
 	images: {
 		contentSize: '(max-width: 860px) 100vw, 620px',
