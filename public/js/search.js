2c097d62f8a5886cd3dcecce20093a6b4b23a25e
Steve Fenton <Steve-Fenton@users.noreply.github.com>
2023-07-04 07:11:19 +0000

    Update accelerator

--- public/js/search.js
+++ public/js/search.js
@@ -5,40 +5,52 @@ import { raiseEvent } from './modules/events.js';
 import { contains, containsWord, sanitise, explode, highlight } from './modules/string.js';
 
 /**
-type Heading = {
-    text: string;
-    safeText: string;
-    slug: string;
-}
+@typedef {
+    {
+        text: string;
+        safeText: string;
+        slug: string;
+    }
+} Heading
 
-type SearchEntry = {
-    score: number;
-    title: string;
-    safeTitle: string;
-    description: string;
-    safeDescription: string;
-    headings: Heading[];
-    tags: string;
-    url: string;
-    date: string;
-    matchedHeadings: Heading[];
-}
+@typedef {
+    {
+        foundWords: number;
+        score: number;
+        title: string;
+        keywords: string;
+        safeTitle: string;
+        description: string;
+        safeDescription: string;
+        headings: Heading[];
+        tags: string[];
+        url: string;
+        date: string;
+        matchedHeadings: Heading[];
+    }
+}  SearchEntry
  */
 
-var dataUrl = qs('#site-search').dataset.sourcedata;
-var haystack = /** @type {SearchEntry} */ [];
+/** @type {SearchEntry[]} */
+var haystack = [];
 var currentQuery = '';
+var dataUrl = qs('#site-search').dataset.sourcedata;
 
 var ready = false;
 var scrolled = false;
 
 /**
- * 
+ * Search term `s` and number of results `r`
  * @param {string} s 
+ * @param {number|null} [r=12]
  * @returns 
  */
-function search(s) {
-    const needles = /** @type {SearchEntry} */ [];
+function search(s, r) {
+    const numberOfResults = r ?? 12;
+    console.log('search', s, numberOfResults);
+
+    /** @type {SearchEntry[]} */
+    const needles =  [];
 
     // Clean the input
     const cleanQuery = sanitise(s);
@@ -52,8 +64,9 @@ function search(s) {
     currentQuery = cleanQuery;
     const queryTerms = explode(currentQuery);
 
-    s.length > 0 && haystack.forEach( (item) => {
+    cleanQuery.length > 0 && haystack.forEach( (item) => {
 
+        let foundWords = 0;
         item.score = 0;
         item.matchedHeadings = [];
 
@@ -82,15 +95,20 @@ function search(s) {
         // Part 2 - Term Matches, i.e. "Kitchen" or "Sink"
         
         queryTerms.forEach(term => {
+            let isTermFound = false;
+
             // Title
             if (contains(item.safeTitle, term)) {
                 item.score = item.score + 40;
+                isTermFound = true;
             }
 
             // Headings
             item.headings.forEach(c => {
                 if (contains(c.safeText, term)) {
                     item.score = item.score + 15;
+                    isTermFound = true;
+
                     if (item.matchedHeadings.filter(h => h.slug == c.slug).length == 0) {
                         item.matchedHeadings.push(c);
                     }
@@ -99,21 +117,30 @@ function search(s) {
 
             // Description
             if (contains(item.description, term)) {
+                isTermFound = true;
                 item.score = item.score + 15;
             }
 
             // Tags
             item.tags.forEach(t => {
                 if (contains(t, term)) {
+                    isTermFound = true;
                     item.score = item.score + 15;
                 }
             });
 
             // Keywords
             if (contains(item.keywords, term)) {
+                isTermFound = true;
                 item.score = item.score + 15;
             }
-        })
+
+            if (isTermFound) {
+                foundWords++;
+            }
+        });
+
+        item.foundWords = foundWords / queryTerms.length;
 
         if (item.score > 0) {
             needles.push(item);
@@ -121,7 +148,11 @@ function search(s) {
     });
 
     needles.sort(function (a, b){
-        return b.score - a.score;
+        if (b.foundWords === a.foundWords) {
+            return b.score - a.score;
+        }
+
+        return b.foundWords - a.foundWords;
     });
 
     const total = needles.reduce(function (accumulator, needle) {
@@ -133,7 +164,7 @@ function search(s) {
     const ol = document.createElement('ol');
     ol.className = 'site-search-results';
 
-    const limit = Math.min(needles.length, 12);
+    const limit = Math.min(needles.length, numberOfResults);
 
     // @ts-ignore
     const siteUrl = new URL(site_url);
@@ -157,25 +188,30 @@ function search(s) {
         markers.className = 'result-text';
         markers.innerHTML = highlight(needle.description, queryTerms);
 
-        const headings = document.createElement('ul');
-        markers.className = 'result-headings';
-        console.log(needle.matchedHeadings);
-        needle.matchedHeadings
-            .forEach(h => {
-                const item = document.createElement('li');
-                const link = document.createElement('a');
-                link.href = url + '#' + h.slug;
-                link.innerHTML = highlight(h.text, queryTerms);
-                item.appendChild(link);
-                headings.append(item);
-            });
-
         const li = document.createElement('li');
+        li.dataset.score = (Math.round((needle.score/ total) * 100)).toString();
         li.appendChild(a);
         li.appendChild(path);
         li.appendChild(markers);
-        li.append(headings);
-        li.dataset.score = (Math.round((needle.score/ total) * 100)).toString();
+
+        if (needle.matchedHeadings.length > 0) {
+            const headings = document.createElement('ul');
+            headings.className = 'result-headings';
+
+            headings.tabIndex = 0;
+
+            needle.matchedHeadings
+                .forEach(h => {
+                    const item = document.createElement('li');
+                    const link = document.createElement('a');
+                    link.href = url + '#' + h.slug;
+                    link.innerHTML = highlight(h.text, queryTerms);
+                    item.appendChild(link);
+                    headings.append(item);
+                });
+
+            li.appendChild(headings);
+        }
 
         ol.appendChild(li);
     }
@@ -185,14 +221,30 @@ function search(s) {
         ? results.dataset.emptytitle || 'No Results'
         : results.dataset.title || 'Results';
 
+    const more = document.createElement('button');
+    more.className = 'show-more';
+    more.type = 'button';
+    more.innerHTML = 'See more';
+    more.addEventListener('click', function() {
+        currentQuery = '';
+        const newTotal = numberOfResults + 12;
+        console.log('More', newTotal);
+        search(s, newTotal);
+    })
+
     results.innerHTML = '';
     results.appendChild(h2);
     results.appendChild(ol);
+
+    if (needles.length > numberOfResults) {
+        results.appendChild(more);
+    }
 
     const address = window.location.href.split('?')[0];
     window.history.pushState({}, '', address + '?q=' + encodeURIComponent(cleanQuery));
 }
 
+/** @type {Number} */
 var debounceTimer;
 
 function debounceSearch() {
@@ -229,9 +281,11 @@ fetch(dataUrl)
             item.headings.forEach(h => h.safeText = sanitise(h.text));
         }
 
-        var siteSearch = qs('#site-search');
+        /** @type {HTMLFormElement} */
+        const siteSearch = qs('#site-search');
 
-        var siteSearchQuery = qs('#site-search-query');
+        /** @type {HTMLInputElement} */
+        const siteSearchQuery = qs('#site-search-query');
 
         if (siteSearch == null || siteSearchQuery == null) {
             throw new Error('Cannot find #site-search or #site-search-query');
@@ -257,7 +311,7 @@ fetch(dataUrl)
 
         const params = new URLSearchParams(window.location.search);
         if (params.has('q')) {
-            siteSearchQuery.value = params.get('q');
+            siteSearchQuery.value = params.get('q') ?? '';
         }
 
         debounceSearch();
