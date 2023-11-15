f4e7c602fffafe64e9b343acbdf14c996135a835
Steve Fenton <Steve-Fenton@users.noreply.github.com>
2023-11-15 13:40:11 +0000

    Add the Astro MDX integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import remarkDirective from 'remark-directive';
 import { defineConfig } from 'astro/config';
 import { defaultLayout } from '/src/themes/accelerator/utilities/default-layout.mjs';
 import { attributeMarkdown, wrapTables } from './src/themes/accelerator/utilities/custom-markdown.mjs';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.stevefenton.co.uk',
+    integrations: [mdx()],
     markdown: {
         shikiConfig: {
             theme: 'nord'
