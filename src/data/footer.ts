af479729d48949f426474a552fb92df265a3137d
Steve Fenton <Steve-Fenton@users.noreply.github.com>
2024-11-07 15:50:12 +0000

    Update footer.ts

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -9,32 +9,37 @@ export const menu: (MenuItem | 'categories' | 'tags' | 'toptags')[] = [
 		children: [{
 			title: '🐘 Mastodon',
 			url: 'https://mastodon.social/@stevefenton',
-			order: 1,
+			order: 10,
+			rel: 'me',
+		},{
+			title: '🦋 Bluesky',
+			url: 'https://bsky.app/profile/steve-fenton.bsky.social',
+			order: 20,
 			rel: 'me',
 		},{
 			title: '🐈 GitHub',
 			url: 'https://github.com/Steve-Fenton',
-			order: 1,
+			order: 30,
 			rel: 'me',
 		},{
 			title: '🏢 LinkedIn',
 			url: 'https://www.linkedin.com/in/stevefenton/',
-			order: 1,
+			order: 40,
 			rel: 'me',
 		},{
 			title: '♾️ DevOps engineer\'s handbook',
 			url: 'https://www.octopus.com/devops/authors/steve-fenton/1/',
-			order: 1,
+			order: 50,
 			rel: 'me',
 		},{
 			title: '📚 The New Stack',
 			url: 'https://thenewstack.io/author/steve-fenton/',
-			order: 1,
+			order: 60,
 			rel: 'me',
 		},{
 			title: '🔮 Medium',
 			url: 'https://medium.com/@steve.fenton',
-			order: 1,
+			order: 70,
 			rel: 'me',
 		}]
 }];
