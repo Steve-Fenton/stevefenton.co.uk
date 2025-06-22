7e8ccdd4196cdf25cf13feb56933fc45b5076e81
Steve Fenton <Steve-Fenton@users.noreply.github.com>
2025-06-22 13:16:48 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.stevefenton.co.uk',
+    site: 'https://stevefenton.co.uk',
     integrations: [mdx()],
     markdown: {
         shikiConfig: {
