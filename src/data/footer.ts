9112c42ac51c2b260ed1afd13437630368b85c4d
Steve Fenton <github@stevefenton.co.uk>
2026-03-21 18:49:17 +0000

    feat: add Coderlegion link to the footer and update the cspell dictionary

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -7,56 +7,61 @@ export const menu: (MenuItem | 'categories' | 'tags' | 'toptags')[] = [
 		title: 'Quick Links',
 		order: 1,
 		children: [{
-		title: '🗼 RSS feed',
-		url: 'https://stevefenton.co.uk/feed.xml',
-		order: 0
-	},{
-		title: '🐘 Mastodon',
-		url: 'https://mastodon.social/@stevefenton',
-		order: 10,
-		rel: 'me',
-	},{
-		title: '🦋 Bluesky',
-		url: 'https://bsky.app/profile/stevefenton.co.uk',
-		order: 20,
-		rel: 'me',
-	},{
-		title: '🐈 GitHub',
-		url: 'https://github.com/Steve-Fenton',
-		order: 30,
-		rel: 'me',
-	},{
-		title: '🏢 LinkedIn',
-		url: 'https://www.linkedin.com/in/stevefenton/',
-		order: 40,
-		rel: 'me',
-	},{
-		title: '♾️ DevOps',
-		url: 'https://www.octopus.com/devops/authors/steve-fenton/1/',
-		order: 50,
-		rel: 'me',
-	},{
-		title: '🐙 Octopus blog',
-		url: 'https://www.octopus.com/blog/authors/steve-fenton/1/',
-		order: 50,
-		rel: 'me',
-	},{
-		title: '📚 The New Stack',
-		url: 'https://thenewstack.io/author/steve-fenton/',
-		order: 60,
-		rel: 'me',
-	},{
-		title: '🔮 Medium',
-		url: 'https://medium.com/@steve.fenton',
-		order: 70,
-		rel: 'me',
-	},{
-		title: '⛽ DEV',
-		url: 'https://dev.to/_steve_fenton_',
-		order: 80,
-		rel: 'me',
-	}]
-}];
+			title: '🗼 RSS feed',
+			url: 'https://stevefenton.co.uk/feed.xml',
+			order: 0
+		}, {
+			title: '🐘 Mastodon',
+			url: 'https://mastodon.social/@stevefenton',
+			order: 10,
+			rel: 'me',
+		}, {
+			title: '🦋 Bluesky',
+			url: 'https://bsky.app/profile/stevefenton.co.uk',
+			order: 20,
+			rel: 'me',
+		}, {
+			title: '🐈 GitHub',
+			url: 'https://github.com/Steve-Fenton',
+			order: 30,
+			rel: 'me',
+		}, {
+			title: '🏢 LinkedIn',
+			url: 'https://www.linkedin.com/in/stevefenton/',
+			order: 40,
+			rel: 'me',
+		}, {
+			title: '♾️ DevOps',
+			url: 'https://www.octopus.com/devops/authors/steve-fenton/1/',
+			order: 50,
+			rel: 'me',
+		}, {
+			title: '🐙 Octopus blog',
+			url: 'https://www.octopus.com/blog/authors/steve-fenton/1/',
+			order: 50,
+			rel: 'me',
+		}, {
+			title: '📚 The New Stack',
+			url: 'https://thenewstack.io/author/steve-fenton/',
+			order: 60,
+			rel: 'me',
+		}, {
+			title: '🔮 Medium',
+			url: 'https://medium.com/@steve.fenton',
+			order: 70,
+			rel: 'me',
+		}, {
+			title: '⛽ DEV',
+			url: 'https://dev.to/_steve_fenton_',
+			order: 80,
+			rel: 'me',
+		}, {
+			title: '💻 Coderlegion',
+			url: 'https://coderlegion.com/user/Steve+Fenton',
+			order: 90,
+			rel: 'me',
+		}]
+	}];
 
 /*
 See navigation.ts
