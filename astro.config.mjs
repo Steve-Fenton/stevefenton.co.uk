a2d971d4fdc065e775a6cee6dee413f9f9550972
Steve Fenton <99181436+steve-fenton-octopus@users.noreply.github.com>
2026-07-15 13:16:02 +0100

    Update Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,31 @@
 import remarkDirective from 'remark-directive';
 import { defineConfig } from 'astro/config';
 import { readingTime } from '/src/themes/accelerator/utilities/reading-time.mjs';
+import { unified } from '@astrojs/markdown-remark';
 import { defaultLayout } from '/src/themes/accelerator/utilities/default-layout.mjs';
 import { attributeMarkdown, wrapTables } from './src/themes/accelerator/utilities/custom-markdown.mjs';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://stevefenton.co.uk',
-    integrations: [mdx()],
-    markdown: {
-        shikiConfig: {
-            theme: 'nord'
-        },
-        remarkPlugins: [
-            defaultLayout,
-            remarkDirective,
-            attributeMarkdown,
-            wrapTables,
-            readingTime,
-        ],
-        extendDefaultPlugins: true,
-        trailingSlash: 'always'
+  site: "https://stevefenton.co.uk",
+  integrations: [mdx()],
+  trailingSlash: "always",
+  markdown: {
+    shikiConfig: {
+      theme: "nord",
     },
-    server: {
-        port: 3000
-    },
+    processor: unified({
+      remarkPlugins: [
+        defaultLayout,
+        remarkDirective,
+        attributeMarkdown,
+        wrapTables,
+        readingTime,
+      ],
+    }),
+  },
+  server: {
+    port: 3000,
+  },
 });
